0dacb60af59e65dbc803ccd9ba2ca0b564d3b830
root <root@MrSlim3.>
2024-04-18 15:26:20 -0500

    check node for root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (0);
+    
+    else if (node->parent == NULL)
+        return (1);
+    else
+        return (0);
+}
