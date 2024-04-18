7a4b6d5a7cdb9fa30ef2e338d298b7c00ea0caaa
root <root@MrSlim3.>
2024-04-18 18:19:52 -0500

    measure height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
     size_t left_height, right_height;
 
     if (tree == NULL)
-        return 0;
+		return 0;
+	
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
-
