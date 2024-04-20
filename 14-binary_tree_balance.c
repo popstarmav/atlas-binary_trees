744b0be7e58a89135af369728c8e9f91d374696a
root <root@MrSlim3.>
2024-04-19 20:09:32 -0500

    measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,7 +5,7 @@
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure the balance factor
- * 
+ *
  * Return: The balance factor of the binary tree
 */
 int binary_tree_balance(const binary_tree_t *tree)
@@ -14,29 +14,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	
+
 	return (left_height - right_height);
 }
 
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
- * 
+ *
  * Return: The balance factor of the binary tree
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
-	
+
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	
+
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
