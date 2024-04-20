805de101a4fba02b8d41baadfb341c578afd4ca2
root <root@MrSlim3.>
2024-04-19 19:05:00 -0500

    countes node in at leat 1 node in binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,16 +4,17 @@
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  * @tree: Point to the root node of the tree to count the number of nodes
- * 
+ *
  * Return: The number of nodes
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    
-    return ((tree->left != NULL) || (tree->right != NULL)) +
-           binary_tree_nodes(tree->left) + 
-           binary_tree_nodes(tree->right);
+	if (tree == NULL)
+		return (0);
+
+	return (((tree->left != NULL) || (tree->right != NULL)) +
+			binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
 }
+
