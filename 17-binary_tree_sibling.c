464901d3457b4e32412e16af3d9559154efeef9e
root <root@MrSlim3.>
2024-04-19 21:08:36 -0500

    tree slibing

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,13 +11,13 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL ||
-			node->parent->left == NULL || 
+			node->parent->left == NULL ||
 			node->parent->right == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	if (node->parent->left == node)
-		return node->parent->right;
+		return (node->parent->right);
 	else
-		return node->parent->left;
+		return (node->parent->left);
 }
 
