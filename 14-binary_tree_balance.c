d19f9a75c24c9cbb2f9046c09036cf82ec136d98
root <root@MrSlim3.>
2024-04-19 19:18:45 -0500

    measures balance in binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,11 +4,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    int left_height, right_height;
+    
     if (tree == NULL)
         return (0);
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
     return (left_height - right_height);
 }
