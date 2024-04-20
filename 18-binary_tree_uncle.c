64cf7c5abc06159c49e30a595c56423f81777477
root <root@MrSlim3.>
2024-04-19 21:04:31 -0500

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,20 +9,20 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || 
+	if (node == NULL || node->parent == NULL ||
 			node->parent->parent == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	if (node->parent->parent->left == node->parent)
 	{
 		if (node->parent->parent->right != NULL)
-			return node->parent->parent->right;
+			return (node->parent->parent->right);
 	}
 	else
 	{
 		if (node->parent->parent->left != NULL)
-			return node->parent->parent->left;
+			return (node->parent->parent->left);
 	}
-	return NULL;
+	return (NULL);
 }
 
