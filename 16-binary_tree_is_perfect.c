5062097d0543b1a1331e8025ab4afebe0377c318
root <root@MrSlim3.>
2024-04-19 20:53:33 -0500

    tree slibing

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,3 +40,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+    
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    
+    if (((tree->left != NULL) && (tree->right != NULL) ||
+    (tree->left == NULL && tree->right == NULL))
+    return (0));
+
+    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
