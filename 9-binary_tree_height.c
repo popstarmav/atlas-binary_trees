8b774b2ce7ba11db3c7896e7c640f1c5292716c2
popstarmav <cj.johnson2017@gmail.com>
2025-03-02 02:32:37 -0600

    fix: adjust binary tree height to return 0 for single node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include "binary_trees.h"
 
@@ -6,7 +5,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t left_height, right_height;
 
-    if (tree == NULL)
+    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
         return (0);
 
     left_height = binary_tree_height(tree->left);
@@ -14,3 +13,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     return (1 + (left_height > right_height ? left_height : right_height));
 }
+
